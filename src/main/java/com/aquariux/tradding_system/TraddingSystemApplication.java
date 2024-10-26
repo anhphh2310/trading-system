8a6c367310c885bf753354c0a29f72b11cb87617
anhphh <anhphh@unifiedpost.com>
2024-10-26 13:43:11 +0700

    feat(source): Initial commit

---
+++ src/main/java/com/aquariux/tradding_system/TraddingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aquariux.tradding_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraddingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraddingSystemApplication.class, args);
+	}
+
+}
